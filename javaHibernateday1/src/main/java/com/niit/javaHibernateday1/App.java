aa61875fb55a06f9c8681ab76a4ef765a1cabd30
hp <bindureddy4755@gmail.com>
2021-04-30 15:44:35 +0530

    fetch data

--- javaHibernateday1/src/main/java/com/niit/javaHibernateday1/App.java
+++ javaHibernateday1/src/main/java/com/niit/javaHibernateday1/App.java
@@ -7,19 +7,24 @@ import org.hibernate.cfg.Configuration;
 
 public class App 
 {
-	 public static void main( String[] args )
-	    {
-	        System.out.println( "Hello World!" );
-	        Student student = new Student(13, "bindu", 3,33);
-	        
-	        Configuration configuration = new Configuration().configure().addAnnotatedClass(Student.class);
-	        
-	        SessionFactory sessionFactory = configuration.buildSessionFactory();
-	        Session session = sessionFactory.openSession();
-	        Transaction transaction = session.beginTransaction();
-	        
-	       session.save(student);
-	       transaction.commit();
-	    }
+	public static void main( String[] args )
+	{
+		System.out.println( "Hello World!" );
+		Student student = new Student(14, "himanireddy", 3,33);
+		//Student student1 = new Student(15, "sindhu", 3,33);
+		//System.out.println(student);
+		Configuration configuration = new Configuration().configure().addAnnotatedClass(Student.class);
+
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+		Student s1 = session.get(Student.class, 12);
+		System.out.println(s1);
+		/*
+		 * session.save(student); session.save(student1);
+		 */
+		transaction.commit();
+
+	}
     
 }
--- javaHibernateday1/src/main/java/com/niit/javaHibernateday1/Student.java
+++ javaHibernateday1/src/main/java/com/niit/javaHibernateday1/Student.java
@@ -4,18 +4,20 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Transient;
-
-@Entity(name = "student")
+@Entity
+//@Table(name = "student_table")
 public class Student {
 	@Id
 	int id;
 	@Transient
+	//@Transient
 	String name;
 	@Column(name = "trimester")
+	//@Column(name = "trimester")
 	int sem;
 	int average;
 
-
+	public  Student() {}
 	public Student(int id, String name, int semester, int average) {
 		super();
 		this.id = id;
@@ -34,20 +36,15 @@ public class Student {
 	}
 	public void setName(String name) {
 		this.name = name;
-	}
-	public int getSemester() {
-		return sem;
-	}
-	public void setSemester(int semester) {
-		this.sem = semester;
-	}
-	public int getAverage() {
-		return average;
-	}
-	public void setAverage(int average) {
+
 		this.average = average;
 	}
 
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "\n"+id+"\t"+name+"\t"+sem+"\t"+average;
+	}
 
 
 }
